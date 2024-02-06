cb5365c8a51b6ccf169a6cfb11ffaab22fa42505
Kor de Jong <k.dejong@geoneric.eu>
2024-02-06 21:30:36 +0100

    Initial work adding tables

--- source/package/adaptation_pathways/desktop/application.py
+++ source/package/adaptation_pathways/desktop/application.py
@@ -1,14 +1,19 @@
 import sys
 
 from PySide6.QtCore import QObject, Slot
-from PySide6.QtGui import QAction, QIcon
+from PySide6.QtGui import QIcon
 from PySide6.QtUiTools import QUiLoader
-from PySide6.QtWidgets import QApplication, QFileDialog
+from PySide6.QtWidgets import QApplication  # , QFileDialog
 
 import adaptation_pathways as ap
 
+from .model.sequence import SequenceModel
+from .model.tipping_point import TippingPointModel
 from .path import Path
 
+
+# pylint: disable-next=wrong-import-order, unused-import
+from __feature__ import snake_case  # isort:skip
 
 loader = QUiLoader()
 
@@ -22,59 +27,62 @@ except ImportError:
     pass
 
 
-class MainUI(QObject):  # Not a widget.
+class MainUI(QObject):  # Not a widget
     def __init__(self):
         super().__init__()
         self.name = "Adaptation Pathway Generator"
         self.version = f"{ap.__version__}"
 
         self.ui = loader.load(Path.ui("main_window.ui"), None)
-        self.ui.setWindowTitle(f"{self.name} - {self.version}")
+        self.ui.window_title = f"{self.name} - {self.version}"
         self.ui.show()
 
-        open_sequences_action = QAction(
-            QIcon(Path.icon("folder-open-table.png")),
-            "&Open sequences table",
-            self,
-        )
-        open_sequences_action.triggered.connect(self.open_sequences_table)
-        quit_action = QAction(
-            "Quit",
-            self,
-        )
-        file_menu = self.ui.menubar.addMenu("&File")
-        file_menu.addAction(open_sequences_action)
-        file_menu.addAction(quit_action)
+        self.ui.action_open.set_icon(QIcon(Path.icon("folder-open-table.png")))
+        self.ui.action_save.set_icon(QIcon(Path.icon("disk.png")))
 
-        about_action = QAction(
-            "About",
-            self,
-        )
-        about_action.triggered.connect(self.show_about_dialog)
-        help_menu = self.ui.menubar.addMenu("&Help")
-        help_menu.addAction(about_action)
+        self.ui.action_open.triggered.connect(self.open_sequences_table)
+        self.ui.action_about.triggered.connect(self.show_about_dialog)
 
-        self.ui.toolBar.addAction(open_sequences_action)
+        # https://www.pythonguis.com/tutorials/qtableview-modelviews-numpy-pandas/
+        # https://www.pythonguis.com/faq/editing-pyqt-tableview/
+
+        data = [
+            ["current", "a"],
+            ["a", "b"],
+            ["b", "c"],
+        ]
+        self.sequence_model = SequenceModel(data)
+        self.ui.table_sequences.set_model(self.sequence_model)
+
+        data = [
+            ["current", 2020],
+            ["a", 2030],
+            ["b", 2040],
+            ["c", 2050],
+        ]
+        self.tipping_point_model = TippingPointModel(data)
+        self.ui.table_tipping_points.set_model(self.tipping_point_model)
 
     @Slot()
     def open_sequences_table(self):
+        pass
         # TODO Open a table (*.txt (see networkx doc for format convention))
-        pathname = QFileDialog.getOpenFileName(
-            self.ui, "Open File", "/home", "Images (*.png *.xpm *.jpg)"
-        )
-        print(pathname)
+        # pathname = QFileDialog.get_open_file_name(
+        #     self.ui, "Open File", "/home", "Images (*.png *.xpm *.jpg)"
+        # )
+        # print(pathname)
 
     @Slot()
     def show_about_dialog(self):
         dialog = loader.load(Path.ui("about_dialog.ui"), self.ui)
-        dialog.setWindowTitle(f"About {self.name}")
-        dialog.text.setText("*Meh*!")
+        dialog.set_window_title(f"About {self.name}")
+        dialog.text.set_text("*Meh*!")
         dialog.show()
 
 
 def application():
     app = QApplication(sys.argv)
-    app.setWindowIcon(QIcon(Path.icon("icon.svg")))
+    app.set_window_icon(QIcon(Path.icon("icon.svg")))
     _ = MainUI()
     app.exec()
 
---
+++ source/package/adaptation_pathways/desktop/model/sequence.py
@@ -0,0 +1,39 @@
+from PySide6 import QtCore  # , QtGui, QtWidgets
+from PySide6.QtCore import Qt
+
+
+# pylint: disable-next=wrong-import-order, unused-import
+from __feature__ import snake_case  # isort:skip
+
+
+# TODO The model can contain (from action, to action) tuples (Action instances, including
+#      multiple revisions of the same action)
+#      When editing, actions can be selected from a list of already defined actions or new ones
+#      can be added.
+#      When removing actions, a warning must be shown when tipping point info will also be removed.
+
+
+class SequenceModel(QtCore.QAbstractTableModel):
+    def __init__(self, data):
+        super().__init__()
+        self._data = data
+        self._header = ["From action", "To action"]
+
+    # pylint: disable=inconsistent-return-statements
+    def data(self, index, role):
+        if role == Qt.DisplayRole:
+            name = self._data[index.row()][index.column()]
+            edition = 0
+            return f"{name}[{edition}]"
+
+    def row_count(self, index):  # pylint: disable=unused-argument
+        return len(self._data)
+
+    def column_count(self, index):  # pylint: disable=unused-argument
+        return len(self._data[0])
+
+    # pylint: disable=inconsistent-return-statements
+    def header_data(self, section, orientation, role):
+        if role == Qt.DisplayRole:
+            if orientation == Qt.Horizontal:
+                return self._header[section]
