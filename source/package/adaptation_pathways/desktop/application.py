a8a118d935e16369c1fcc7c8048e6a6476242242
Kor de Jong <k.dejong@geoneric.eu>
2024-02-16 11:53:20 +0100

    Make it possible to edit sequences table

--- source/package/adaptation_pathways/desktop/application.py
+++ source/package/adaptation_pathways/desktop/application.py
@@ -1,7 +1,8 @@
+import copy
 import sys
 from io import StringIO
 
-from PySide6 import QtGui, QtWidgets
+from PySide6 import QtCore, QtGui, QtWidgets
 from PySide6.QtCore import QObject, Slot
 from PySide6.QtUiTools import QUiLoader
 
@@ -20,6 +21,9 @@ from .model.sequence import SequenceModel
 from .path import Path
 from .widget.pathway_map import PathwayMapWidget
 from .widget.sequence_graph import SequenceGraphWidget
+
+
+# pylint: disable=too-many-instance-attributes, too-many-locals
 
 
 loader = QUiLoader()
@@ -57,13 +61,23 @@ class MainUI(QObject):  # Not a widget
             lambda idx: self.edit_action(idx.row())
         )
 
-        self.actions: list[tuple[Action, Colour, int]] = []
+        self.ui.table_sequences.customContextMenuRequested.connect(
+            self.on_sequences_table_context_menu
+        )
+        self.ui.table_sequences.doubleClicked.connect(
+            lambda idx: self.edit_sequence(idx.row())
+        )
+
+        self.actions: list[list] = []
         self.action_model = ActionModel(self.actions)
         self.ui.table_actions.setModel(self.action_model)
 
-        self.sequences: list[tuple[Action, Action]] = []
-        self.sequence_model = SequenceModel(self.sequences)
+        self.sequences: list[list[Action]] = []
+        self.colour_by_action: dict[Action, Colour] = {}
+        self.sequence_model = SequenceModel(self.sequences, self.colour_by_action)
         self.ui.table_sequences.setModel(self.sequence_model)
+
+        self.action_model.rowsAboutToBeRemoved.connect(self.actions_about_to_be_removed)
 
         # TODO Plot the data from the models, using our own plot routines
         # - Finish refactoring our plot routines
@@ -71,11 +85,11 @@ class MainUI(QObject):  # Not a widget
         sequence_graph_widget = SequenceGraphWidget(
             parent=None, width=5, height=4, dpi=100
         )
-        sequence_graph_widget.axes.plot([0, 1, 2, 3, 4], [10, 1, 20, 3, 40])
+        # sequence_graph_widget.axes.plot([0, 1, 2, 3, 4], [10, 1, 20, 3, 40])
         self.ui.plot_tab_widget.addTab(sequence_graph_widget, "Sequence graph")
 
         pathway_map_widget = PathwayMapWidget(parent=None, width=5, height=4, dpi=100)
-        pathway_map_widget.axes.plot([4, 3, 2, 1, 0], [10, 1, 20, 3, 40])
+        # pathway_map_widget.axes.plot([4, 3, 2, 1, 0], [10, 1, 20, 3, 40])
         self.ui.plot_tab_widget.addTab(pathway_map_widget, "Pathway map")
 
         self.ui.editor_tab_widget.setCurrentIndex(0)
@@ -94,6 +108,8 @@ class MainUI(QObject):  # Not a widget
             )
         )
         colours = default_action_colours(len(actions))
+        self.colour_by_action.clear()
+        self.colour_by_action.update(zip(actions, colours))
         tipping_points = [0] * len(actions)
 
         sequences = read_sequences(
@@ -102,7 +118,8 @@ class MainUI(QObject):  # Not a widget
                 current a
                 a b
                 b c"""
-            )
+            ),
+            actions,
         )
         # level_by_action = action_level_by_first_occurrence(sequences)
         # sequence_graph = sequences_to_sequence_graph(sequences)
@@ -122,11 +139,13 @@ class MainUI(QObject):  # Not a widget
         # pathway_map.set_attribute("level", level_by_action)
 
         self.actions.clear()
-        self.actions.extend(zip(actions, colours, tipping_points))
+        self.actions.extend(list(t) for t in zip(actions, colours, tipping_points))
+        # TODO try to use the model logic for this
         self.ui.table_actions.model().layoutChanged.emit()
 
         self.sequences.clear()
         self.sequences.extend(sequences)
+        # TODO try to use the model logic for this
         self.ui.table_sequences.model().layoutChanged.emit()
 
         # TODO Open a dataset
@@ -136,27 +155,30 @@ class MainUI(QObject):  # Not a widget
 
     def on_actions_table_context_menu(self, pos):
         context = QtWidgets.QMenu(self.ui.table_actions)
-
         action_idx = self.ui.table_actions.rowAt(pos.y())
 
-        if action_idx != -1:
-            edit_action_action = QtGui.QAction("Edit action...", self.ui.table_actions)
-            edit_action_action.triggered.connect(lambda: self.edit_action(action_idx))
-            context.addAction(edit_action_action)
+        edit_action_action = QtGui.QAction("Edit action...", self.ui.table_actions)
+        edit_action_action.triggered.connect(lambda: self.edit_action(action_idx))
+        context.addAction(edit_action_action)
+        edit_action_action.setEnabled(action_idx != -1)
 
-            remove_action_action = QtGui.QAction("Remove action", self.ui.table_actions)
-            remove_action_action.triggered.connect(
-                lambda: self.remove_action(action_idx)
-            )
-            context.addAction(remove_action_action)
+        remove_action_action = QtGui.QAction("Remove action", self.ui.table_actions)
+        remove_action_action.triggered.connect(
+            lambda: self.remove_actions(action_idx, 1)
+        )
+        context.addAction(remove_action_action)
+        remove_action_action.setEnabled(action_idx != -1)
 
         add_action_action = QtGui.QAction("Add action...", self.ui.table_actions)
         add_action_action.triggered.connect(self.add_action)
         context.addAction(add_action_action)
 
         clear_actions_action = QtGui.QAction("Clear actions", self.ui.table_actions)
-        clear_actions_action.triggered.connect(self.clear_actions)
+        clear_actions_action.triggered.connect(
+            lambda: self.remove_actions(0, len(self.actions))
+        )
         context.addAction(clear_actions_action)
+        clear_actions_action.setEnabled(len(self.actions) > 0)
 
         context.exec(self.ui.table_actions.viewport().mapToGlobal(pos))
 
@@ -170,8 +192,8 @@ class MainUI(QObject):  # Not a widget
         self.edit_action(len(self.actions) - 1)
 
     def edit_action(self, idx):
-        action_tuple = self.actions[idx]
-        action, colour, tipping_point = action_tuple
+        action_record = self.actions[idx]
+        action, colour, tipping_point = action_record
 
         dialog = loader.load(Path.ui("edit_action_dialog.ui"), self.ui)
         dialog.name_edit.setText(action.name)
@@ -210,21 +232,237 @@ class MainUI(QObject):  # Not a widget
             )
 
             if something_changed:
-                new_action_tuple = (
-                    Action(new_name),
-                    new_colour.getRgbF(),
-                    new_tipping_point,
-                )
-                self.actions[idx] = new_action_tuple
+                # self.actions[idx][0].name = new_name
+                # self.actions[idx][1] = new_colour.getRgbF()
+                # self.actions[idx][2] = new_tipping_point
+
+                self.actions[idx] = [action, new_colour.getRgbF(), new_tipping_point]
+
+                old_name = action.name
+
+                for sequence in self.sequences:
+                    from_action, to_action = sequence
+                    if from_action.name == old_name:
+                        from_action.name = new_name
+                    if to_action.name == old_name:
+                        to_action.name = new_name
+
+                self.actions[idx][0].name = new_name
+
+                self.colour_by_action[action] = new_colour.getRgbF()
+
+                # new_action_tuple = (
+                #     Action(new_name),
+                #     new_colour.getRgbF(),
+                #     new_tipping_point,
+                # )
+                # self.actions[idx] = new_action_tuple
+                # TODO try to use the model logic for this
                 self.ui.table_actions.model().layoutChanged.emit()
+                self.ui.table_sequences.model().layoutChanged.emit()
 
-    def remove_action(self, idx):
-        del self.actions[idx]
-        self.ui.table_actions.model().layoutChanged.emit()
+    def remove_actions(self, idx, nr_actions):
+        # TODO button = QtWidgets.QMessageBox.warning(
+        # TODO     self.ui,
+        # TODO     "Warning",
+        # TODO     "Removing actions will also remove associated sequences.\n" "Are you sure?",
+        # TODO     QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.Cancel,
+        # TODO )
 
-    def clear_actions(self):
-        self.actions.clear()
-        self.ui.table_actions.model().layoutChanged.emit()
+        # TODO if button == QtWidgets.QMessageBox.Yes:
+        # TODO     self.ui.table_actions.model().removeRows(
+        # TODO         idx, nr_actions, parent=QtCore.QModelIndex()
+        # TODO     )
+
+        # TODO Warning or not? Don't annoy people
+        self.ui.table_actions.model().removeRows(idx, nr_actions, QtCore.QModelIndex())
+
+    def actions_about_to_be_removed(
+        self, parent, first_idx, last_idx
+    ):  # pylint: disable=unused-argument
+        # Handle the situation that actions are about to be removed from the table:
+        # - Sequences that involve the actions must be removed as well
+        # - Colours associated with the actions must be removed
+        actions = [record[0] for record in self.actions[first_idx : last_idx + 1]]
+        action_names = [action.name for action in actions]
+        sequences = [
+            record
+            for record in self.sequences
+            if record[0].name in action_names or record[1].name in action_names
+        ]
+
+        for sequence in sequences:
+            self.ui.table_sequences.model().removeRow(self.sequences.index(sequence))
+
+        for action in actions:
+            del self.colour_by_action[action]
+
+    def on_sequences_table_context_menu(self, pos):
+        context = QtWidgets.QMenu(self.ui.table_sequences)
+        sequence_idx = self.ui.table_sequences.rowAt(pos.y())
+
+        edit_sequence_action = QtGui.QAction(
+            "Edit sequence...", self.ui.table_sequences
+        )
+        edit_sequence_action.triggered.connect(lambda: self.edit_sequence(sequence_idx))
+        context.addAction(edit_sequence_action)
+        edit_sequence_action.setEnabled(sequence_idx != -1)
+
+        remove_sequence_action = QtGui.QAction(
+            "Remove sequence", self.ui.table_sequences
+        )
+        remove_sequence_action.triggered.connect(
+            lambda: self.ui.table_sequences.model().removeRow(sequence_idx)
+        )
+        context.addAction(remove_sequence_action)
+        remove_sequence_action.setEnabled(sequence_idx != -1)
+
+        add_sequence_action = QtGui.QAction("Add sequence...", self.ui.table_sequences)
+        add_sequence_action.triggered.connect(self.add_sequence)
+        context.addAction(add_sequence_action)
+        add_sequence_action.setEnabled(len(self.actions) > 1)
+
+        clear_sequences_action = QtGui.QAction(
+            "Clear sequences", self.ui.table_sequences
+        )
+        clear_sequences_action.triggered.connect(
+            lambda: self.ui.table_sequences.model().removeRows(
+                0, len(self.sequences), parent=QtCore.QModelIndex()
+            )
+        )
+        context.addAction(clear_sequences_action)
+        clear_sequences_action.setEnabled(len(self.sequences) > 0)
+
+        context.exec(self.ui.table_sequences.viewport().mapToGlobal(pos))
+
+    def add_sequence(self):
+        assert len(self.actions) > 1
+        from_action = self.actions[0][0]
+        to_action = self.actions[1][0]
+
+        self.sequences.append([from_action, to_action])
+        # TODO try to use the model logic for this
+        self.ui.table_sequences.model().layoutChanged.emit()
+        self.edit_sequence(len(self.sequences) - 1)
+
+    def edit_sequence(self, idx):  # pylint: disable=too-many-statements
+        sequence_record = self.sequences[idx]
+        from_action, to_action = sequence_record
+
+        dialog = loader.load(Path.ui("edit_sequence_dialog.ui"), self.ui)
+
+        dialog.start_pathway_radio_button.toggled.connect(
+            dialog.from_action_start_combo_box.setEnabled
+        )
+        dialog.continue_pathway_radio_button.toggled.connect(
+            dialog.from_action_continue_combo_box.setEnabled
+        )
+
+        actions = [record[0] for record in self.actions]
+        sequences = self.sequences
+        to_actions = [record[1] for record in self.sequences]
+
+        start_of_pathway = from_action not in to_actions
+        continuation_of_pathway = not start_of_pathway
+
+        dialog.start_pathway_radio_button.setChecked(start_of_pathway)
+        dialog.from_action_start_combo_box.setEnabled(start_of_pathway)
+        dialog.continue_pathway_radio_button.setChecked(continuation_of_pathway)
+        dialog.from_action_continue_combo_box.setEnabled(continuation_of_pathway)
+
+        # To start a pathway, one of the existing actions must be selected
+        # To end a sequence, one of the existing actions must be selected
+        for action in actions:
+            image = QtGui.QPixmap(16, 16)
+            image.fill(QtGui.QColor.fromRgbF(*self.colour_by_action[action]))
+            dialog.from_action_start_combo_box.addItem(image, action.name)
+            dialog.to_action_combo_box.addItem(image, action.name)
+
+        # To continue a pathway, one of the defined sequences must be selected
+        for sequence in sequences:
+            from_action_, to_action_ = sequence
+            image = QtGui.QImage(16, 16, QtGui.QImage.Format_RGB32)
+            painter = QtGui.QPainter(image)
+            painter.fillRect(
+                0, 0, 8, 16, QtGui.QColor.fromRgbF(*self.colour_by_action[from_action_])
+            )
+            painter.fillRect(
+                8, 0, 8, 16, QtGui.QColor.fromRgbF(*self.colour_by_action[to_action_])
+            )
+            painter.end()
+            dialog.from_action_continue_combo_box.addItem(
+                QtGui.QPixmap.fromImage(image),
+                f"{from_action_.name} - {to_action_.name}",
+            )
+
+        if start_of_pathway:
+            dialog.from_action_start_combo_box.setCurrentIndex(
+                next(
+                    idx
+                    for idx, action in enumerate(actions)
+                    if action.name == sequence_record[0].name
+                )
+            )
+        else:
+            # Find sequence that ends in current from_action
+            from_sequence = next(
+                sequence for sequence in sequences if sequence[1] == from_action
+            )
+            dialog.from_action_continue_combo_box.setCurrentIndex(
+                sequences.index(from_sequence)
+            )
+
+        dialog.to_action_combo_box.setCurrentIndex(
+            next(
+                idx
+                for idx, action in enumerate(actions)
+                if action.name == sequence_record[1].name
+            )
+        )
+
+        if dialog.exec():
+            start_of_pathway = dialog.start_pathway_radio_button.isChecked()
+            continuation_of_pathway = not start_of_pathway
+
+            if start_of_pathway:
+                action_idx = dialog.from_action_start_combo_box.currentIndex()
+
+                if actions[action_idx] == from_action:
+                    # Nothing changed
+                    new_from_action = from_action
+                else:
+                    # Deep copy the existing action
+                    new_from_action = copy.deepcopy(actions[action_idx])
+                    self.colour_by_action[new_from_action] = self.colour_by_action[
+                        actions[action_idx]
+                    ]
+            else:
+                sequence_idx = dialog.from_action_continue_combo_box.currentIndex()
+
+                # Reference to the existing action
+                new_from_action = sequences[sequence_idx][1]
+
+            action_idx = dialog.to_action_combo_box.currentIndex()
+
+            if actions[action_idx] == to_action:
+                # Nothing changed
+                new_to_action = to_action
+            else:
+                # Deep copy the existing action
+                new_to_action = copy.deepcopy(actions[action_idx])
+                self.colour_by_action[new_to_action] = self.colour_by_action[
+                    actions[action_idx]
+                ]
+
+            something_changed = (
+                new_from_action != from_action or new_to_action != to_action
+            )
+
+            if something_changed:
+                self.sequences[idx] = [new_from_action, new_to_action]
+
+                # TODO try to use the model logic for this
+                self.ui.table_sequences.model().layoutChanged.emit()
 
     @Slot()
     def show_about_dialog(self):
--- source/package/adaptation_pathways/desktop/model/action.py
+++ source/package/adaptation_pathways/desktop/model/action.py
@@ -1,58 +1,32 @@
-from PySide6 import QtCore, QtGui  # , QtWidgets
+from PySide6 import QtCore, QtGui
 from PySide6.QtCore import Qt
-
-from ...action import Action
-from ...plot.colour import Colour
 
 
 class ActionModel(QtCore.QAbstractTableModel):
 
-    _actions: list[tuple[Action, Colour, int]]
-    _headers: tuple[str, str, str]
+    _actions: list[list]
+    _headers: tuple[str, str]
 
-    def __init__(self, actions: list[tuple[Action, Colour, int]]):
+    def __init__(self, actions: list[list]):
 
         super().__init__()
         self._actions = actions
-        self._headers = ("Name", "Colour", "Tipping point")
-
-    # def flags(self, index):
-    #     _flags = Qt.ItemIsSelectable | Qt.ItemIsEnabled
-
-    #     return _flags | Qt.ItemIsEditable if index.column() != 1 else _flags
+        self._headers = ("Name", "Tipping point")
 
     # pylint: disable=inconsistent-return-statements, no-else-return
     def data(self, index, role):
-        if role in (Qt.DisplayRole, Qt.EditRole):
+        if role == Qt.DisplayRole:
             record = self._actions[index.row()]
             if index.column() == 0:
                 action = record[0]
                 return action.name
-            elif index.column() == 2:
+            elif index.column() == 1:
                 tipping_point = record[2]
                 return f"{tipping_point}"
-            else:
-                return ""
-        elif role == Qt.BackgroundRole:
-            if index.column() == 1:
+        elif role == Qt.DecorationRole:
+            if index.column() == 0:
                 colour = self._actions[index.row()][1]
                 return QtGui.QColor.fromRgbF(*colour)
-
-    # def setData(self, index, value, role):
-    #     if role == Qt.EditRole:
-    #         record = self._actions[index.row()]
-    #         if index.column() == 0:
-    #             action = record[0]
-    #             action.name = value
-    #             return True
-    #         elif index.column() == 1:
-    #             # TODO
-    #             pass
-    #             return True
-    #         else:
-    #             self._actions[index.row()] = \
-    #                 (*record[:index.column()], int(value), *record[index.column():])
-    #             return True
 
     def rowCount(self, index):  # pylint: disable=unused-argument
         return len(self._actions)
@@ -65,3 +39,9 @@ class ActionModel(QtCore.QAbstractTableModel):
         if role == Qt.DisplayRole:
             if orientation == Qt.Horizontal:
                 return self._headers[section]
+
+    def removeRows(self, row, nr_rows, parent):  # pylint: disable=unused-argument
+        self.beginRemoveRows(QtCore.QModelIndex(), row, row + nr_rows - 1)
+        del self._actions[row : row + nr_rows]
+        self.endRemoveRows()
+        return True
