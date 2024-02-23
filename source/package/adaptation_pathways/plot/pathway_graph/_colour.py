1360f6fe886b3e4bc05a03a0c9d8d2fcbabe25dd
Kor de Jong <k.dejong@geoneric.eu>
2024-02-23 13:06:50 +0100

    Add pathway graph

--- source/package/adaptation_pathways/plot/pathway_graph/_colour.py
+++ source/package/adaptation_pathways/plot/pathway_graph/_colour.py
@@ -1,6 +1,7 @@
 from ...graph.node import ActionConversion, ActionPeriod
 from ...graph.pathway_graph import PathwayGraph
 from ..colour import (
+    Colour,
     Colours,
     PlotColours,
     default_edge_colours,
@@ -12,31 +13,44 @@ from ..colour import (
 )
 
 
-def default_node_colours(graph: PathwayGraph) -> Colours:
-    palette = default_nominal_palette()
-
-    palette_size = len(palette)
-    conversion_colour = nord_palette_light[0] + (default_transparency(),)
-
-    colour_by_action_name = {}
+def colour_by_node(
+    graph: PathwayGraph, colour_by_action_name: dict[str, Colour]
+) -> Colours:
+    # pylint: disable=redefined-outer-name
     colours = []
+    conversion_colour = nord_palette_light[0] + (default_transparency(),)
 
     # Use the same colour for conversions
     # Colour each unique action unique
-    idx = 0
-
     for node in graph._graph.nodes:
-        # assert type(node) in [Action, ActionConversion]
         assert type(node) in [ActionConversion, ActionPeriod]
         if isinstance(node, ActionPeriod):
-            if node.action.name not in colour_by_action_name:
-                colour_by_action_name[node.action.name] = palette[idx % palette_size]
-                idx += 1
             colours.append(colour_by_action_name[node.action.name])
         elif isinstance(node, ActionConversion):
             colours.append(conversion_colour)
 
     return colours
+
+
+def colour_by_action_name(graph: PathwayGraph, palette: Colours) -> dict[str, Colour]:
+    palette_size = len(palette)
+    result = {}
+    idx = 0
+
+    for node in graph._graph.nodes:
+        assert type(node) in [ActionConversion, ActionPeriod]
+        if isinstance(node, ActionPeriod):
+            if node.action.name not in result:
+                result[node.action.name] = palette[idx % palette_size]
+                idx += 1
+
+    return result
+
+
+def default_node_colours(graph: PathwayGraph) -> Colours:
+    return colour_by_node(
+        graph, colour_by_action_name(graph, default_nominal_palette())
+    )
 
 
 def default_colours(pathway_graph: PathwayGraph) -> PlotColours:
