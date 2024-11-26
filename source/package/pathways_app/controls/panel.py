3d05421456dd3fcfff479c314d010df742b9b14e
AJ Kolenc <ajkolenc@gmail.com>
2024-11-26 13:49:43 +0100

    Fix flet linting and directory structure

--- source/package/pathways_app/controls/panel.py
+++ source/package/pathways_app/controls/panel.py
@@ -1,15 +1,16 @@
 import flet as ft
 import theme
 
+
 class Panel(ft.Container):
     def __init__(self, content=None):
         super().__init__(
-            expand=True,             
+            expand=True,
             margin=0,
-            padding=ft.padding.symmetric(8,8),
+            padding=ft.padding.symmetric(8, 8),
             bgcolor=theme.colors.primary_white,
-            border = ft.border.all(1, theme.colors.primary_light),
-            border_radius = theme.variables.small_radius,
+            border=ft.border.all(1, theme.colors.primary_light),
+            border_radius=theme.variables.small_radius,
             content=content,
         )
         # [
@@ -25,5 +26,5 @@ class Panel(ft.Container):
         #         right=0,
         #         top=0,
         #         bottom=0
-        #     ) 
+        #     )
         # ]
