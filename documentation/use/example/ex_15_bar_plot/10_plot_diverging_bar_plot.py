9d4eda11bed3fd0ee8fdae9b5d2377323ff34b49
Kor de Jong <k.dejong@geoneric.eu>
2025-02-07 16:38:52 +0100

    Add bar plots to documentation

---
+++ documentation/use/example/ex_15_bar_plot/10_plot_diverging_bar_plot.py
@@ -0,0 +1,58 @@
+"""
+Pathway map for diverging pathways
+==================================
+"""
+
+from io import StringIO
+
+import matplotlib.pyplot as plt
+
+from adaptation_pathways.graph import SequenceGraph, sequence_graph_to_pathway_map
+from adaptation_pathways.io import text
+from adaptation_pathways.plot import (
+    action_level_by_first_occurrence,
+    init_axes,
+    plot_bars,
+)
+
+
+actions, colour_by_action = text.read_actions(
+    StringIO(
+        """
+current #ff4c566a
+a #ffbf616a
+b #ffd08770
+c #ffebcb8b
+"""
+    )
+)
+sequences, tipping_point_by_action = text.read_sequences(
+    StringIO(
+        """
+current current 2030
+current a 2040
+current b 2050
+current c 2060
+"""
+    ),
+    actions,
+)
+sequence_graph = SequenceGraph(sequences)
+pathway_map = sequence_graph_to_pathway_map(sequence_graph)
+
+level_by_action = action_level_by_first_occurrence(sequences)
+colour_by_action_name = {
+    action.name: colour for action, colour in colour_by_action.items()
+}
+
+pathway_map.assign_tipping_points(tipping_point_by_action)
+pathway_map.set_attribute("level_by_action", level_by_action)
+
+arguments = {
+    "colour_by_action_name": colour_by_action_name,
+}
+
+_, axes = plt.subplots(layout="constrained")
+init_axes(axes)
+plot_bars(axes, pathway_map, arguments=arguments)
+plt.show()
---
+++ documentation/use/example/ex_15_bar_plot/25_plot_example_bar_plot.py
@@ -0,0 +1,75 @@
+"""
+Pathway map for an example
+==========================
+"""
+
+from io import StringIO
+
+import matplotlib.pyplot as plt
+
+from adaptation_pathways.graph import SequenceGraph, sequence_graph_to_pathway_map
+from adaptation_pathways.io import text
+from adaptation_pathways.plot import (
+    action_level_by_first_occurrence,
+    init_axes,
+    plot_bars,
+)
+
+
+actions, colour_by_action = text.read_actions(
+    StringIO(
+        """
+current #ff4c566a
+a #ffbf616a
+b #ffd08770
+c #ffebcb8b
+d #ffa3be8c
+"""
+    )
+)
+sequences, tipping_point_by_action = text.read_sequences(
+    StringIO(
+        """
+current     current     2030
+
+current     a[1]        2100
+
+current     b[1]        2040
+b[1]        a[2]        2100
+b[1]        c[2]        2050
+c[2]        b[2]        2070
+b[2]        a[3]        2100
+c[2]        a[4]        2100
+c[2]        d[3]        2100
+b[1]        d[2]        2100
+
+current     c[1]        2050
+c[1]        b[3]        2070
+b[3]        a[5]        2100
+c[1]        a[6]        2100
+c[1]        d[4]        2100
+
+current     d[1]        2100
+"""
+    ),
+    actions,
+)
+sequence_graph = SequenceGraph(sequences)
+pathway_map = sequence_graph_to_pathway_map(sequence_graph)
+
+level_by_action = action_level_by_first_occurrence(sequences)
+colour_by_action_name = {
+    action.name: colour for action, colour in colour_by_action.items()
+}
+
+pathway_map.assign_tipping_points(tipping_point_by_action)
+pathway_map.set_attribute("level_by_action", level_by_action)
+
+arguments = {
+    "colour_by_action_name": colour_by_action_name,
+}
+
+_, axes = plt.subplots(layout="constrained")
+init_axes(axes)
+plot_bars(axes, pathway_map, arguments=arguments)
+plt.show()
